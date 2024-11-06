c8740426c8c02e1a39e3a5fad4afccb5fa3b9dcf
DESKTOP-0OQJJ1L\AreumKang <areumkang87@gmail.com>
2024-11-06 23:52:30 +0900

    분석함수

--- workspace_sql_2023/src/오라클_SQL과_PL_SQL을_다루는_기술/제7장 복잡한 연산결과를 추출해내는 고급쿼리 다루기.sql
+++ workspace_sql_2023/src/오라클_SQL과_PL_SQL을_다루는_기술/제7장 복잡한 연산결과를 추출해내는 고급쿼리 다루기.sql
@@ -208,6 +208,22 @@ order by LEVEL
 
 
 ---------------------------------
+/* 24.5.29 복습 Start */
+with T_empList as ( 
+	select * from employees
+	where rownum <= 3
+)
+--로우를 컬럼으로 변환
+, T_toCol as ( 
+	select LISTAGG(EMP_NAME, ', ') WITHIN GROUP (order by EMP_NAME) AS row_to_col 
+	from T_empList
+)
+selct * from T_toCol 
+;
+
+--컬럼을 로우로 변환 
+
+/**/
 
 --# 2) 로우를 컬럼으로 변환하기 (LISTAGG)
 --LISTAGG(expr, delimiter) WITHIN GROUP(ORDER BY절) --> expr을 delimiter로 구분해서 로우를 컬럼으로 변환해 조회하는 함수.
@@ -231,7 +247,7 @@ group by manager_id
 --# 3) 컬럼을 로우로 변환하기 (계층형 쿼리 사용)
 select REPLACE(SUBSTR(empnames, start_pos, end_pos - start_pos), ',' , '') as emp_name
 		--SUBSTR(empnames, start_pos, end_pos)
-		--INSTR 사용법 : INSTR(컬럼명, '찾을문자', 1:앞에서부터검색, 몇번째 찾은위치)
+		--INSTR ( [문자열], [찾을 문자 값], [찾기를 시작할 위치(1,-1)], [찾은 결과의 순번(1...n)] )
 from ( 
 		select empnames
 			 , LEVEL as lvl
@@ -397,6 +413,47 @@ select parent_id, department_id, LPAD(' ', 3*(lvl-1)) || department_name as  dep
 from recur; 
 
 
+--/////////////////////// s
+--## WITH절 다시 공부하기 (2024.10.20)
+--요건1) kor_loan_status 테이블에서, 연도별 최종월 기준 가장 대출이 많은 도시와 잔액 구하기 
+
+select * from kor_loan_status ;
+
+--연도별 최종월 구하기
+/*
+201112
+201212
+201311*/
+select max(period) from kor_loan_status
+group by substr(PERIOD,1,4) 
+order by substr(PERIOD,1,4)
+;
+
+--최종년월별 가장 대출이 많은 잔액 구하기
+select PERIOD, max(LOAN_JAN_AMT) from kor_loan_status 
+where period in (select max(period) from kor_loan_status
+                  group by substr(PERIOD,1,4))
+group by period 
+;
+
+
+--최종년월별 가장 대출이 많은 도시와 잔액 구하기
+select a.* from KOR_LOAN_STATUS a 
+              , (select PERIOD, max(LOAN_JAN_AMT) max_jan 
+                   from kor_loan_status 
+                  where period in (select max(period) --년도별 최종월 
+                                     from kor_loan_status
+                                    group by substr(PERIOD,1,4))
+                  group by period ) b
+where A.PERIOD = B.PERIOD
+  and A.LOAN_JAN_AMT = b.max_jan 
+;
+
+--검증
+select * from KOR_LOAN_STATUS
+where loan_jan_amt in (204275.7, 203344.9, 205644.3)
+;
+--////////////////////////// e
 
 
 
@@ -410,5 +467,79 @@ from recur;
  * 이에반해, 집계함수를 사용하면 로우의 손실없이도 그룹별 집계값을 산출해 낼 수 있다.
  * 
  * 분석함수에서 사용하는 로우별 그룹을 ★윈도우(window)라고 부르는데, 이는 집계 값 계산을 위한 로우의 범위를 결정하는 역할★을 한다.   
+ * 
+ * [분석함수 구문]
+ *  분석함수(매개변수) OVER
+ *  (PARTITION BY expr1, expr2, ...
+ *       ORDER BY expr3, expr4...
+ *    window 절)
  * */
+
+--분석함수 : 분석함수 역시 특정 그룹별 집계를 담당하므로 집계함수에 속한다. 
+--1. ROW_NUMBER()
+--요건: 사원 텡비르에서 부서별 사원들의 로우수를 출력하기
+select A.DEPARTMENT_ID
+      ,A.EMP_NAME 
+      ,ROW_NUMBER() over(partition by department_id order by DEPARTMENT_ID, A.EMP_NAME) RN
+  from employees a
+;
+
+--RANK, DENSE_RANK
+--RANK: 순서별로 중복있으면 건너뛰고 순서매김
+--DENSE_RANK 중복있어도 쭉이어서 순서매김
+
+--부서별로 급여순위를 매겨보자
+select DEPARTMENT_ID, emp_name, SALARY
+     , rank() over(partition by department_id order by salary) RN 
+ from EMPLOYEES
+where department_id = '50'
+;
+
+select DEPARTMENT_ID, emp_name, SALARY
+     , dense_rank() over(partition by department_id order by salary) RN 
+ from EMPLOYEES
+where department_id = '50'
+;
+
+--각 부서별로 급여가 상위 3위까지인 사원 추출
+select department_id, salary, sal_rank, emp_name from (
+    select DEPARTMENT_ID, emp_name, SALARY
+         , rank() over(partition by department_id order by salary desc) sal_rank 
+     from EMPLOYEES
+) where sal_rank <= 3
+order by department_id, sal_rank 
+;
+
+
+
+--##CUME_DIST, PERCENT_RANK
+--CUME_DIST : 주어진 그룹안에서 상대적인 분포도값 반환 (0초과 1이하)
+
+--부서별 급여에 따른 "누적"분포도값을 구해보자
+select department_id, emp_name, salary
+      ,CUME_DIST() over(partition by DEPARTMENT_ID order by salary) dep_dist
+from EMPLOYEES ;
+
+--PERCENT_RANK : 해당그룹 내의 백분위 순위 (0이상 1이하) 
+--*백분위 순위란: 그룹 안에서 해당 로우의 값도가 작은값의 비율.
+select department_id, emp_name, salary
+      ,dense_rank() over(partition by department_id order by salary) sal_rank 
+      ,CUME_DIST() over(partition by DEPARTMENT_ID order by salary) dep_dist
+      ,PERCENT_RANK() over(partition by DEPARTMENT_ID order by salary) percentile
+from EMPLOYEES 
+where department_id = '60' 
+;
+
+--## NTILE(expr) : expr안에 명시된 값만큼 분할한 결과를 반환 
+--e.g 1) 한그룹의 row가 5일때: NTILE(5): 1에서 5까지 숫자를 반환(1,2,3,4,5)
+--e.g 2) 한그룹의 row가 5일때: NTILE(4): 1에서 5까지 숫자를 반환(12,3,4,5)
+select department_id, emp_name, salary
+     , NTILE(4) over(partition by department_id order by salary) NTILES 
+from EMPLOYEES
+where department_ID IN (30,60)
+;
+
+--## LAG(expr, offset, default_value) | LEAD(expr, offset, default_value)
+--LAG  : 선행로우 참조
+--LEAD : 후행로우 참조 
 
